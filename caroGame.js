1d211c79476f50332c4cea5a8ba52f1d367f4aed
LilDarz <DarkLegend710@gmail.com>
2022-02-28 16:39:33 +0700

    update Caro Game

--- caroGame.js
+++ caroGame.js
@@ -1,94 +1,101 @@
+const VALUE_EMPTY = 1;
+const VALUE_X = 2;
+const VALUE_O = 3;
+const DEFAULT_COLS = 10;
+const DEFAULT_ROWS = 10;
+const DEFAULT_CELL_SIZE = 40;
 
-
-function Board(x, y) {
+function Cell(x, y) {
     this.x = x;
     this.y = y;
-    this.value = function () {
-        let top = x * DEFAULT_CELL_SIZE;
-        let left = y * DEFAULT_CELL_SIZE;
-        let cellHtml = '< div id = "cell-'+x+'-'+y+'" onclick = "play('+x+','+y+')" class="cell" style = "position: absolute; width: '+
-        DEFAULT_CELL_SIZE + 'px; height:' +
-            DEFAULT_CELL_SIZE + 'px; left:' +
-            left + 'px; top:' +
-            top + 'px; line-height: ' +
-            DEFAULT_CELL_SIZE + 'px;"></div>';
+    this.value = VALUE_EMPTY;
+    this.getHtml = function(){
+        var top = x * DEFAULT_CELL_SIZE;
+        var left = y * DEFAULT_CELL_SIZE;
+        var cellHtml = '<div id="cell-'+x+'-'+y+'" onclick="play('+x+','+y+')" class="cell" style="position: absolute; width: '+
+            DEFAULT_CELL_SIZE+'px; height:'+
+            DEFAULT_CELL_SIZE+'px; left:'+
+            left+'px; top:'+
+            top+'px; line-height: '+
+            DEFAULT_CELL_SIZE+'px;"></div>';
         return cellHtml;
     };
-};
-
-this.draw = function () {
-    let cellDiv = document.getElementById("cell -"+x+"-"+y);
-    switch (this.value) {
-        case VALUE_X:
-            cellDiv.innerHTML = "X";
-            break;
-        case VALUE_O:
-            cellDiv.innerHTML = "O";
-            break
-        default:
-            cellDiv.innerHTML = "";
-            break;
+    
+    this.draw = function () {
+        var cellDiv = document.getElementById("cell-"+x+"-"+y);
+        switch (this.value){
+            case VALUE_X:
+                cellDiv.innerHTML = "X";
+                break;
+            case VALUE_O:
+                cellDiv.innerHTML = "O";
+                break;
+            default:
+                cellDiv.innerHTML = "";
+                break;
+        }
     }
 }
 
-function GameBoard(rows,cols, elementId) {
+function GameBoard(rows, cols, elementId) {
     this.rows = rows;
     this.cols = cols;
     this.elementId = elementId;
     this.turn = VALUE_O;
-    this.cell = [];
+    this.cells = [];
     this.isOver = false;
-
+    
     this.draw = function () {
-        let gameBoardDiv = document.getElementById(this.elementId);
+        var gameBoardDiv = document.getElementById(this.elementId);
         gameBoardDiv.innerHTML = "";
-        for(let i=0; i<this.rows;i++){
-            let row = [];
-            this.cell.push(row);
-            for(let j = 0; j<this.cols; j++){
-                let cell= new Board(i,j)
+        for(var i = 0; i < this.rows; i++){
+            var row = [];
+            this.cells.push(row);
+            for(var j = 0; j < this.cols; j++){
+                var cell = new Cell(i, j);
                 row.push(cell);
                 gameBoardDiv.innerHTML += cell.getHtml();
             }
         }
-    }
+    };
 
-    this.play = function (x,y) {
-        if(this.isOver){
+    this.play = function (x, y) {
+        if(this.isOver) {
             return;
         }
-        let cell = this.cells[x][y];
+        var cell = this.cells[x][y];
         if(cell.value === VALUE_EMPTY){
             cell.value = this.turn;
             cell.draw();
-            this.check(x,y);
-            if (this.turn===VALUE_O) {
+            this.check(x, y);
+            if(this.turn === VALUE_O){
                 this.turn = VALUE_X;
-            }else{
-                this.turn= VALUE_O;
+            } else {
+                this.turn = VALUE_O;
             }
-        }else {
+        } else {
             alert("Cell is not empty");
         }
-    }
-
-    this.check = function (x,y) {
-        let cell = this.cells[x][y];
-        let count = 1;
-        let i = 1;
-        while((y+1<this.cols)&&this.cells[x][y + i].value ===  cell.value){
+    };
+    
+    this.check = function (x, y) {
+        var cell = this.cells[x][y];
+        //Horizontal
+        var count = 1;
+        var i = 1;
+        while((y + i < this.cols) && this.cells[x][y + i].value ===  cell.value){
             count++;
             i++;
         }
-        let i=1;
+        var i = 1;
         while((y - i >= 0) && this.cells[x][y - i].value ===  cell.value){
             count++;
             i++;
         }
         this.endGame(count);
-
-        let count = 1;
-        let i=1;
+        //Vertical
+        var count = 1;
+        var i = 1;
         while((x + i < this.rows) &&this.cells[x + i][y].value ===  cell.value){
             count++;
             i++;
@@ -134,23 +141,22 @@ function GameBoard(rows,cols, elementId) {
         }
         this.endGame(count);
     };
-    
-    this.endGame = function (count) {
-        if(count>=4){
-            this.isOver = true;
-            alert("You won!!");
-        }
-    }
-};
 
-function play(x,y) {
-    gameBoard.play(x,y);
+    this.endGame = function (count) {
+        if(count >= 5){
+            this.isOver = true;
+            alert("You won!!!");
+        }
+    };
+}
+
+function play(x, y) {
+   gameBoard.play(x, y);
 }
 
 function start() {
-    gameBoard=new GameBoard(DEFAULT_ROWS, DEFAULT_COLS,"game-board");
+    gameBoard = new GameBoard(DEFAULT_ROWS, DEFAULT_COLS, "game-board");
     gameBoard.draw();
 }
-
-let gameBoard;
-start()
+var gameBoard;
+start();
